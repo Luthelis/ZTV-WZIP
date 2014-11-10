d4cadd66509fcc580a748d6c2c93344a4884a50b
Timothy Transue <timothytransue@Timothys-MacBook-Pro-4.local>
2014-11-10 10:24:51 -0500

    Second working commit.  Published for TestFlight.

--- ZTV WZIP/SocialMediaTableViewController.m
+++ ZTV WZIP/SocialMediaTableViewController.m
@@ -47,7 +47,7 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 //#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 3;
+    return 2;
 }
 
 /*
